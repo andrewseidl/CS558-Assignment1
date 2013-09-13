cbfc9839344ba6f47992ae9cb8ca9f8f4321cddd
Andrew Seidl <git@aas.io>
2013-09-13 15:47:35 -0500

    Longer test case

--- test/test.js
+++ test/test.js
@@ -5,12 +5,12 @@ var stepSimulator = require("../simulator.js")
 var position = [[0, 0.5]]
 var velocity = [[0, 1.0]]
 var ground   = [0, 1]
-var dt       = 0.1
+var dt       = 0.15
 
-for(var t=0.0; t<1.0; t+=dt) {
+for(var t=0.0; t<5.0; t+=dt) {
     var nstate = stepSimulator(position, velocity, ground, dt)
         position = nstate.position
         velocity = nstate.velocity
         assert.equal(position[0][0], 0.0)
-        assert.ok(almostEqual(position[0][1], 1.0 - Math.abs((t+dt) - 0.5), almostEqual.DBL_EPSILON))
+        assert.ok(almostEqual(position[0][1], 1.0 - Math.abs((t+dt) - 0.5), almostEqual.FLT_EPSILON))
 }
