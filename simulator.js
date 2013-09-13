aa6ad3e7242ed084d2624a7098fef28885578cb7
Andrew Seidl <git@aas.io>
2013-09-13 17:09:12 -0500

    reworked intersection code, should work

--- simulator.js
+++ simulator.js
@@ -1,8 +1,5 @@
-// TODO: styleguide, should these be in the function?
-// NOTE: assuming convex polygon for the container
-var Iterator = require("iterator").Iterator
+// 'vectors' might not have been the best choice
 var vectors = require("vectors")
-var eqs = require("eqsolver")
 
 var add = vectors.add(2)
   , mult = vectors.mult(2)
@@ -12,8 +9,34 @@ var add = vectors.add(2)
   , dot = vectors.dot(2)
   , normalize = vectors.normalize(2)
   , mag = vectors.mag(2)
+  , dist = vectors.dist(2)
 
 module.exports = simulator
+
+function det(p1, p2) {
+    return p1[0]*p2[1]-p1[1]*p2[0]
+}
+
+function intersection(s1, e1, s2, e2) {
+    var p = s1.slice(0);
+    var r = e1.slice(0);
+    sub(r,s1);
+
+    var q = s2.slice(0);
+    var s = e2.slice(0);
+    sub(s,s2);
+
+    var crosss = cross(r,s);
+    sub(q,p);
+    var numer = cross(q,s);
+    var t = numer/crosss;
+    mult(r,t)
+    add(p,r);
+
+    return p;
+}
+
+
 
 function simulator(position, velocity, ground, dt) {
 
@@ -34,9 +57,12 @@ function simulator(position, velocity, ground, dt) {
     for (var i = 0; i < nextPosition.length; i++) {
         // hmm, seems like vectors does things inplace, work off copies
         // calculate new positions
+        var prevPoint = nextPosition[i].slice(0)
         add(nextPosition[i], mult(copy(nextVelocity[i]), dt))
 
-            // check if point is inside polygon
+        var remainingTime = dt
+
+            // check if point is inside (convex) polygon
         for (var j =0; j < poly.length; j++) {
             var p1 = poly[j].slice(0)
             var p2 = poly[(j+1)%poly.length].slice(0)
@@ -46,43 +72,36 @@ function simulator(position, velocity, ground, dt) {
             sub(v1, p1)
             sub(v2, nextPosition[i])
 
-            if (cross(copy(v1), v2) > 0) {
+            if (det(v2,v1) < 0) {
                 // TODO: better logging framework/see if console has 'debug'/'info' streams
                 // more 'informational'/'debug' than typical log
-                console.log("    Point:   " + nextPosition[i] + "\n    Crosses: " + p1 + "\n             " + p2) 
+                //console.log("    Point:   " + nextPosition[i] + "\n    Crosses: " + p1 + "\n             " + p2) 
                 //mirror the point across the line
                 //from: http://stackoverflow.com/questions/8954326/how-to-calculate-the-mirror-point-along-a-line
                 var A = p2[1] - p1[1]
                 var B = -(p2[0] - p1[0])
                 var C = -A * p1[0] - B * p1[0]
 
-                var D = B
-                var E = -A
-
-                var F = -D*nextPosition[i][0] - E*nextPosition[i][1]
-
-                lhs = [[A,B,0],[D,E,0],[0,0,0]]
-                rhs = [-C,-F,0]
-
-                console.log(rhs)
-                console.log(lhs)
-                var np = eqs(lhs,rhs)
-                //np = rhs
-                console.log("moo: " + np.b)
-
-                nextPosition[i][0] = 2*np.a - nextPosition[i][0]
-                nextPosition[i][1] = 2*np.b - nextPosition[i][1]
-
                 var V = nextVelocity[i].slice(0)
-       
                 var N = normalize([-A, -B])
+
                 // 'vectors' seems a bit ugly
-                mult(N,dot(N,V)/dot(N,N))
-                mult(N,2)
+                mult(N,2*dot(N,V)/dot(N,N))
                 sub(nextVelocity[i], N)
-                //mult(nextVelocity[i],-1)
+                //console.log("    New Velocity: " + nextVelocity[i])
+
+                // find intersection
+                var inters = intersection(p1, p2, nextPosition[i], prevPoint)
+                //console.log("    Intersection: " + inters + "\n")
+
+                remainingTime = remainingTime*(1-dist(prevPoint,inters)/dist(nextPosition[i],prevPoint))
+
+                prevPoint = nextPosition[i].slice(0)
+                nextPosition[i] = inters.slice(0)
+                add(nextPosition[i], mult(copy(nextVelocity[i]), remainingTime))
+                //console.log("    New point:   " + nextPosition[i])
+
                 
-                console.log("    New Velocity: " + nextVelocity[i] + "\n")
 
                 j = 0 //we've moved the point, need to recheck to make sure we don't cross any other sides
             }
