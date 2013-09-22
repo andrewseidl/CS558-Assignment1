f52eb9a68293e199ae07c12db82a5e293523298e
Andrew Seidl <mahasher@andrewseidl.com>
2013-09-22 00:31:46 -0500

    Fixing drawing issues, refactor to functions

--- visualizer/visualizer.js
+++ visualizer/visualizer.js
@@ -13,6 +13,7 @@ var nstate = sim(position, velocity, ground, dt, poly, polyHasGround)
 
 var myCanvas = document.body.querySelector("#visuals")
 var velocs = document.body.querySelector("#velocity")
+var posField = document.body.querySelector("#positions")
 
 myCanvas.width = 400
 myCanvas.height = 400
@@ -26,30 +27,64 @@ var context = myCanvas.getContext("2d")
 console.log(nstate.poly.length)
 
 function drawBox() {
-for (var i=0; i < nstate.poly.length; i++) {
-    context.moveTo(scale*(-topLeft[0]+nstate.poly[i][0]),scale*(topLeft[1]-nstate.poly[i][1]))
-    context.lineTo(scale*(-topLeft[0]+nstate.poly[(i+1)%nstate.poly.length][0]),scale*(topLeft[1]-nstate.poly[(i+1)%nstate.poly.length][1]))
+    context.beginPath()
+    for (var i=0; i < nstate.poly.length; i++) {
+        context.moveTo(scale*(-topLeft[0]+nstate.poly[i][0]),scale*(topLeft[1]-nstate.poly[i][1]))
+        context.lineTo(scale*(-topLeft[0]+nstate.poly[(i+1)%nstate.poly.length][0]),scale*(topLeft[1]-nstate.poly[(i+1)%nstate.poly.length][1]))
+    }
+
+    context.strokeStyle = "#000"
+    context.stroke();
 }
 
-context.strokeStyle = "#000"
-context.stroke();
+function transformCoords(x,y,scale,tL) {
+    return [scale*(x-tL[0]), scale*(tL[1]-y)]
+}
+
+function drawPoint(point) {
+    context.beginPath();
+    context.arc(point[0], point[1], 5, 0,2*Math.PI, true);
+    context.closePath();
+    context.fill();
 }
 
 function drawPoints() {
     //requestAnimationFrame(drawPoints)
-    context.clearRect(0,0,myCanvas.width,myCanvas.height)
-        drawBox()
-    nstate = sim(position, velocity, ground, dt, poly.slice(0), polyHasGround)
-        position = nstate.position
-        velocity = nstate.velocity
+    context.clearRect(0,0,myCanvas.width,myCanvas.height);
+    drawBox();
+    nstate = sim(position, velocity, ground, dt, poly.slice(0), polyHasGround);
+    position = nstate.position;
+    velocity = nstate.velocity;
 
     for (var i=0; i < nstate.position.length; i++) {
-        context.beginPath();
-        context.arc(scale*(nstate.position[i][0]-topLeft[0]),scale*(topLeft[1]-nstate.position[i][1]),10,0,2*Math.PI);
-        //context.arc(100,100,10,0,2*Math.PI);
-        context.closePath();
+        drawPoint(transformCoords(nstate.position[i][0], nstate.position[i][1], scale, topLeft))
         velocs.innerHTML = nstate.velocity[i]
     }
 }
 
-setInterval(drawPoints,1000*dt)
+var simulate = setInterval(drawPoints,1000*dt)
+
+function parseField(data) {
+    //regex:
+    // -?        : optional minus sign
+    // [\d\.]+   : one or more digits or .
+    // [^\d\.]+  : one or more characters that aren't digits or .
+    return data.match(/-?[\d\.]+[^\d\.]+-?[\d\.]+/g).map(function(x){return x.split(/[^\d\.-]/)})
+}
+
+function checkPositions(event) {
+    clearInterval(simulate);
+    console.log("Cleared")
+    context.clearRect(0,0,myCanvas.width,myCanvas.height);
+    context.clearRect(0,0,myCanvas.width,myCanvas.height);
+    context.clearRect(0,0,myCanvas.width,myCanvas.height);
+    context.clearRect(0,0,myCanvas.width,myCanvas.height);
+    drawBox();
+    data = parseField(posField.value)
+    for (var i=0; i < data.length; i++) {
+        drawPoint(transformCoords(1.0*data[i][0],1.0*data[i][1],scale, topLeft));
+    }
+}
+
+posField.addEventListener("change",checkPositions)
+
